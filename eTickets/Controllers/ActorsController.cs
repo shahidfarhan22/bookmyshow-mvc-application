d323e7a38c790be534e7baa6a29c2e1f170719d6
shahidfarhan22 <shahidfarhan2207@gmail.com>
2025-07-03 07:14:33 +0530

    actor model completed

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -11,11 +11,11 @@ namespace eTickets.Controllers
         public ActorsController(IActorsService service)
 
         {
-			_service = service;
+            _service = service;
         }
         public async Task<IActionResult> Index()
         {
-            var data =await  _service.GetAllAsync();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
         //Get: Actors/Create
@@ -25,7 +25,7 @@ namespace eTickets.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("ProfilePictureURL, FullName, Bio")]Actor actor)
+        public async Task<IActionResult> Create([Bind("ProfilePictureURL, FullName, Bio")] Actor actor)
         {
             if (!ModelState.IsValid)
             {
@@ -40,33 +40,53 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Details(int id)
         {
-            var actorDetails  = await _service.GetByIdAsync(id);
+            var actorDetails = await _service.GetByIdAsync(id);
 
-            if (actorDetails == null) return View("Empty");
+            if (actorDetails == null) return View("NotFound");
             return View(actorDetails);
         }
 
-		//Get: Actors/Create
-		public async Task<IActionResult> Edit(int id)
-		{
-			var actorDetails = await _service.GetByIdAsync(id);
-			if (actorDetails == null) return View("Empty");
+        //Get: Actors/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
 
 
-			return View(actorDetails);
-		}
+            return View(actorDetails);
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Edit(int id, [Bind("Id, ProfilePictureURL, FullName, Bio")] Actor actor)
-		{
-			if (!ModelState.IsValid)
-			{
-				return View(actor);
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, ProfilePictureURL, FullName, Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
 
-			}
-			await _service.UpdateAsync(id, actor);
-			return RedirectToAction(nameof(Index));
-		}
+            }
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
+        }
+        //Get: Actors/Edit/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        
+        }
 
-	}
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            await _service.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+    }
 }
